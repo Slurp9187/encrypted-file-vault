2cd3d7a363cecb234ba6f1f9a972e75d88f73a00
Slurp9187 <Slurp9187@users.noreply.github.com>
2025-12-04 03:10:25 -0700

    updated crypto_tests

--- tests/crypto_tests.rs
+++ tests/crypto_tests.rs
@@ -1,73 +1,56 @@
-// tests/core/crypto.rs
-use encrypted_file_vault::aliases::FilePassword;
+// tests/crypto_tests.rs
+use encrypted_file_vault::aliases::{FilePassword, PlainText};
 use encrypted_file_vault::consts::AESCRYPT_V3_HEADER;
 use encrypted_file_vault::crypto::*;
 use encrypted_file_vault::error::CoreError;
 use encrypted_file_vault::file_ops::{aescrypt_version, is_aescrypt_file};
 use encrypted_file_vault::key_ops::generate_key;
-use encrypted_file_vault::SecureConversionsExt;
+use secure_gate::SecureConversionsExt;
 
 #[test]
 fn test_encrypt_decrypt_roundtrip_in_memory() {
-    let plaintext = b"Attack at dawn!";
+    let plaintext = PlainText::new(b"Attack at dawn!".to_vec());
     let key = generate_key();
     let password = FilePassword::new(key.expose_secret().to_hex());
-    let ciphertext = encrypt_to_vec(plaintext, &password).unwrap();
+
+    let ciphertext = encrypt_to_vec(&plaintext, &password).unwrap();
     let decrypted = decrypt_to_vec(&ciphertext, &password).unwrap();
-    assert!(ciphertext.starts_with(b"AES"));
-    assert_eq!(plaintext.as_slice(), decrypted.as_slice());
+
+    assert!(ciphertext.expose_secret().starts_with(b"AES"));
+    assert_eq!(plaintext.expose_secret(), decrypted.expose_secret());
 }
 
 #[test]
 fn test_encrypt_to_vec_produces_v3_header() {
-    let ciphertext = encrypt_to_vec(
-        b"small",
-        &FilePassword::new(generate_key().expose_secret().to_hex()),
-    )
-    .unwrap();
-    assert_eq!(&ciphertext[..5], *AESCRYPT_V3_HEADER);
+    let plaintext = PlainText::new(b"small".to_vec());
+    let password = FilePassword::new(generate_key().expose_secret().to_hex());
+
+    let ciphertext = encrypt_to_vec(&plaintext, &password).unwrap();
+    assert_eq!(&ciphertext.expose_secret()[..5], *AESCRYPT_V3_HEADER);
 }
 
 #[test]
 fn test_is_aescrypt_file_and_version() {
-    let ciphertext = encrypt_to_vec(
-        b"test",
-        &FilePassword::new(generate_key().expose_secret().to_hex()),
-    )
-    .unwrap();
-    assert!(is_aescrypt_file(&ciphertext));
+    let plaintext = PlainText::new(b"test".to_vec());
+    let password = FilePassword::new(generate_key().expose_secret().to_hex());
+
+    let ciphertext = encrypt_to_vec(&plaintext, &password).unwrap();
+
+    assert!(is_aescrypt_file(ciphertext.expose_secret()));
     assert!(!is_aescrypt_file(b"not aes"));
-    assert_eq!(aescrypt_version(&ciphertext), Some(3));
-}
-
-#[test]
-fn test_ensure_v3_returns_error_on_invalid_legacy() {
-    let legacy = vec![0x41, 0x45, 0x53, 0x00, 0x00, 0x00, 0x00, 0x00];
-    let result = ensure_v3(legacy, &FilePassword::new("wrong".to_owned()));
-    assert!(result.is_err());
-}
-
-#[test]
-fn test_ensure_v3_passes_through_v3_unchanged() {
-    let v3 = encrypt_to_vec(
-        b"v3",
-        &FilePassword::new(generate_key().expose_secret().to_hex()),
-    )
-    .unwrap();
-    let result = ensure_v3(v3.clone(), &FilePassword::new("any".to_owned())).unwrap();
-    assert_eq!(v3, result);
+    assert_eq!(aescrypt_version(ciphertext.expose_secret()), Some(3));
 }
 
 #[test]
 fn test_rotate_key_produces_different_ciphertext_and_new_key() {
-    let plaintext = b"secret message";
+    let plaintext = PlainText::new(b"secret message".to_vec());
     let old_key = generate_key();
     let old_password = FilePassword::new(old_key.expose_secret().to_hex());
-    let original = encrypt_to_vec(plaintext, &old_password).unwrap();
 
+    let original = encrypt_to_vec(&plaintext, &old_password).unwrap();
     let (new_ciphertext, new_key) = rotate_key(&original, &old_password).unwrap();
 
-    assert_ne!(original, new_ciphertext);
+    assert_ne!(original.expose_secret(), new_ciphertext.expose_secret());
     assert_ne!(
         old_key.expose_secret().as_slice(),
         new_key.expose_secret().as_slice()
@@ -78,17 +61,20 @@ fn test_rotate_key_produces_different_ciphertext_and_new_key() {
         &FilePassword::new(new_key.expose_secret().to_hex()),
     )
     .unwrap();
-    assert_eq!(plaintext.as_slice(), decrypted.as_slice());
+    assert_eq!(plaintext.expose_secret(), decrypted.expose_secret());
 }
 
 #[test]
 fn test_decrypt_fails_with_wrong_password() {
-    let plaintext = b"secret";
+    let plaintext = PlainText::new(b"secret".to_vec());
     let key1 = generate_key();
     let key2 = generate_key();
 
-    let ciphertext =
-        encrypt_to_vec(plaintext, &FilePassword::new(key1.expose_secret().to_hex())).unwrap();
+    let ciphertext = encrypt_to_vec(
+        &plaintext,
+        &FilePassword::new(key1.expose_secret().to_hex()),
+    )
+    .unwrap();
 
     let wrong = decrypt_to_vec(
         &ciphertext,
